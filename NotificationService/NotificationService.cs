fd5838bbe41b49e64fc1cead8e096254245a0888
Greg <gregbuckle@gmail.com>
2017-06-09 16:16:57 +0100

    Changes to NotificationServices

--- NotificationService/NotificationService.cs
+++ NotificationService/NotificationService.cs
@@ -7,6 +7,7 @@ using System.Configuration;
 using System.Net.Http;
 using System.Security.Cryptography;
 using System.Globalization;
+using System.Web;
 
 namespace IAmFine.Notification
 {
@@ -14,28 +15,29 @@ namespace IAmFine.Notification
     {
         public void SendNotification(string MessageText, string MessageSubject)
         {
-
+            string uriText = HttpUtility.UrlEncode(MessageText);
+            string uriSubject = HttpUtility.UrlEncode(MessageSubject);
             int LineFeedChar = 0x0A;
             string strKEY = ConfigurationManager.AppSettings["NOTIFICATION_ACCESS_KEY_ID"];
             string strAccountID = ConfigurationManager.AppSettings["NOTIFICATION_ACCOUNT_ID"];
             string strPrivateKey = ConfigurationManager.AppSettings["NOTIFICATION_PRIVATE_KEY"];
-            string strTimeStamp = DateTime.Now.Year + "-" + DateTime.Now.Month + "-" + DateTime.Now.Day + "T" + DateTime.Now.Hour + "%3A" + DateTime.Now.Minute + "%3A" + DateTime.Now.Second + "Z";
-            string strReqParams = "AWSAccessKeyId=" + strKEY + "Action=Publish&Message=" + MessageText + "&SignatureMethod=HmacSHA256&SignatureVersion=2&Subject=" + MessageSubject + "&Timestamp=" + strTimeStamp + "&TopicArn=arn%3Aaws%3Asns%3Aus-west-2%3A" + strAccountID + "%3ANSCHackathon";
+            string strTimeStamp = HttpUtility.UrlEncode($"{DateTime.UtcNow:yyyy-MM-ddTHH:mm:ssZ}");
+            string strReqParams = "AWSAccessKeyId=" + strKEY + "&Action=Publish&Message=" + uriText + "&SignatureMethod=HmacSHA256&SignatureVersion=2&Subject=" + uriSubject + "&Timestamp=" + strTimeStamp + "&TopicArn=arn%3Aaws%3Asns%3Aus-west-2%3A" + strAccountID + "%3ANSCHackathon";
             string strRequest = "GET" + Char.ConvertFromUtf32(LineFeedChar) + "sns.us-west-2.amazonaws.com" + Char.ConvertFromUtf32(LineFeedChar) + "/" + Char.ConvertFromUtf32(LineFeedChar) + strReqParams + Char.ConvertFromUtf32(LineFeedChar);
-            string strHash = HashHMACHex(strPrivateKey, strRequest); //convert to URI 
-            System.Uri uriHash = new System.Uri(strHash);
+            string strHash = HashHMACHex(strPrivateKey, strRequest); 
+            string uriHash = HttpUtility.UrlEncode(strHash);
             string SNSURL = "http://sns.us-west-2.amazonaws.com/?" + strReqParams + "&Signature=" + uriHash;
 
             var client = new HttpClient();
 
             Task<string> task = client.GetStringAsync(SNSURL);
-            task.Wait();
+            string output = task.Result;
         }
 
         private static string HashHMACHex(string keyHex, string message)
         {
-            byte[] hash = HashHMAC(HexDecode(keyHex), StringEncode(message));
-            return HashEncode(hash);
+            byte[] hash = HashHMAC(StringEncode(keyHex), StringEncode(message));
+            return Convert.ToBase64String(hash);
         }
 
         private static byte[] HashHMAC(byte[] key, byte[] message)
@@ -47,8 +49,8 @@ namespace IAmFine.Notification
 
         private static byte[] StringEncode(string text)
         {
-            var encoding = new ASCIIEncoding();
-            return encoding.GetBytes(text);
+            var encoding = System.Text.Encoding.UTF8.GetBytes(text);
+            return encoding;
         }
 
         private static string HashEncode(byte[] hash)
@@ -58,6 +60,8 @@ namespace IAmFine.Notification
 
         private static byte[] HexDecode(string hex)
         {
+            return hex.ToCharArray().Select(x=>(byte)x).ToArray();
+
             var bytes = new byte[hex.Length / 2];
             for (int i = 0; i < bytes.Length; i++)
             {
